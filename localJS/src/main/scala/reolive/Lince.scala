6d719b655a594c9dec13f6f41d79d9793302af7a
Jose Proenca <jose@proenca.org>
2019-05-16 23:13:09 +0100

    tweaks to layout of Lince

--- localJS/src/main/scala/reolive/Lince.scala
+++ localJS/src/main/scala/reolive/Lince.scala
@@ -56,16 +56,16 @@ object Lince extends{
     errors = new OutputArea //(id="Lince")
     inputBox = new LinceBox(reload(),"",errors)
     examples = new LinceExamplesBox(softReload(),inputBox,descr)
-    deviation = new InputBox(reloadGraphics(),"0.1","deviation",1,
-      title = "Perturbation warnings",
-      refreshLabel = "Add warnings when conditions would differ when deviating the variables by some value > 0. Set to 0 to ignore these warnings.")
+    deviation = new InputBox(reloadGraphics(),"0","deviation",1,
+      title = "Maximum perturbation",
+      refreshLabel = "Add warnings when conditions would differ when deviating the variables by some perturbation > 0. Set to 0 to ignore these warnings.")
     graphics = new GraphicBox(inputBox,deviation,errors)
 
     inputBox.init(leftColumn,true)
     errors.init(leftColumn)
     examples.init(leftColumn,true)
     descr.init(leftColumn)
-    deviation.init(leftColumn,false)
+    //deviation.init(leftColumn,false)
     graphics.init(rightColumn,visible = true)
 
     // load default button
