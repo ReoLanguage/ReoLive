2fd5992a689e190eef553a091620407b7f51c67b
José Proença <jose@proenca.org>
2022-04-21 12:03:56 +0100

    updated production script - not using standalone, and using sbt instead.

--- production.sh
+++ production.sh
@@ -1,13 +1,19 @@
 echo "Make sure you first modify the 'server/conf/application.conf' file."
 echo "Modify the session key 'play.http.secret.key'."
 echo ""
-echo "## Removing previous standalone"
-rm -rf server/target/universal/server-1.0*
+# echo "## Removing previous standalone"
+# rm -rf server/target/universal/server-1.0*
+echo "## Removing previous compilation"
+rm -rf server/target
 echo "## Building standalone - assuming 'compile.sh' was executed at least once to generate links."
 sbt -mem 2048 fullOptJS server/compile dist
-echo "## Unzipping standalone"
-cd server/target/universal
-yes | unzip server-1.0.zip
-cd ../../../
-echo "## Executing  the server - './server/target/univeral/server-1.0/bin/server"
-./server/target/universal/server-1.0/bin/server -J-Xms2048m -J-Xmx2048m -Dlog4j2.formatMsgNoLookups=true
+echo "## Running the server"
+sbt -mem 2048 -J-Xms2048m -J-Xmx2048m -Dlog4j2.formatMsgNoLookups=true server/run
+
+### Problem 
+# echo "## Unzipping standalone"
+# cd server/target/universal
+# yes | unzip server-1.0.zip
+# cd ../../../
+# echo "## Executing  the server - './server/target/univeral/server-1.0/bin/server"
+# ./server/target/universal/server-1.0/bin/server -J-Xms2048m -J-Xmx2048m -Dlog4j2.formatMsgNoLookups=true
